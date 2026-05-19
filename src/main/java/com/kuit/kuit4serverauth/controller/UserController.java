0c28ce29156158f537c655d82ac6f5378e53d377
Lee Hyeon Seung <arale3488@gmail.com>
2026-05-19 14:23:12 +0900

    feat: JWT 인증/인가 구현

--- src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
@@ -12,12 +12,17 @@ public class UserController {
     @GetMapping("/profile")
     public ResponseEntity<String> getProfile(HttpServletRequest request) {
         // TODO : 로그인 한 사용자면 username 이용해 "Hello, {username}" 반환하기
+        String username = (String) request.getAttribute("username");
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
     }
 
     @GetMapping("/admin")
     public ResponseEntity<String> getAdmin(HttpServletRequest request) {
         // TODO: role이 admin이면 "Hello, admin" 반환하기
+        String role = (String) request.getAttribute("role");
+        if (!"ROLE_ADMIN".equals(role)) {
+            throw new CustomException(ErrorCode.FORBIDDEN_ACCESS);
+        }
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Forbidden");
     }
 }
