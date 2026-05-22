aeaca21afdd043d45a9a9d928435745aaea2fb83
Donghyun Kim <newdhkim0615@gmail.com>
2026-05-22 16:16:27 +0900

    feat: Argument Resolver를 이용한 인증 사용자 정보 주입 구현
    - @LoginUser 커스텀 어노테이션 추가
    - LoginUserResolver 구현 (Interceptor에서 저장한 username, role을 AuthUser로 반환)
    - AuthUser 모델 추가 (username, role 보유)
    - WebConfig에 LoginUserResolver 등록
    - UserController에서 HttpServletRequest 제거, @LoginUser AuthUser로 교체

--- src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.kuit.kuit4serverauth.controller;
 
+import com.kuit.kuit4serverauth.annotation.LoginUser;
+import com.kuit.kuit4serverauth.model.AuthUser;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,9 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/profile")
-    public ResponseEntity<String> getProfile(HttpServletRequest request) {
+    public ResponseEntity<String> getProfile(@LoginUser AuthUser authUser) {
         // TODO : 로그인 한 사용자면 username 이용해 "Hello, {username}" 반환하기
-        String username = (String) request.getAttribute("username");
+        String username = authUser.getUsername();
         if (username != null)
             return ResponseEntity.ok("Hello, " + username);
 
@@ -20,9 +22,9 @@ public class UserController {
     }
 
     @GetMapping("/admin")
-    public ResponseEntity<String> getAdmin(HttpServletRequest request) {
+    public ResponseEntity<String> getAdmin(@LoginUser AuthUser authUser) {
         // TODO: role이 admin이면 "Hello, admin" 반환하기
-        String role = (String) request.getAttribute("role");
+        String role = authUser.getRole();
         if (role != null && role.equals("ROLE_ADMIN"))
             return ResponseEntity.ok("Hello, admin");
 
