6da86239f1bf10f5a7cc5260afaf7b0f46e225de
ajm6238 <ajm9668@naver.com>
2026-05-22 17:47:36 +0900

    /profile , /admin 구현 완료

--- src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
@@ -2,6 +2,7 @@ package com.kuit.kuit4serverauth.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,12 +13,26 @@ public class UserController {
     @GetMapping("/profile")
     public ResponseEntity<String> getProfile(HttpServletRequest request) {
         // TODO : 로그인 한 사용자면 username 이용해 "Hello, {username}" 반환하기
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+
+       String username = (String) request.getAttribute("username");
+       if (username != null){
+           String s = "Hello " + username;
+           return ResponseEntity.status(HttpStatus.FOUND).body(s);
+       }
+
+        else return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
     }
 
     @GetMapping("/admin")
     public ResponseEntity<String> getAdmin(HttpServletRequest request) {
         // TODO: role이 admin이면 "Hello, admin" 반환하기
+
+        String role = (String)request.getAttribute("role");
+        if ("admin".equals(role)){
+            String s = "Hello" + role;
+            return ResponseEntity.status(HttpStatus.FOUND).body(s);
+        }
+
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Forbidden");
     }
 }
